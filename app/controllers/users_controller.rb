c95899e84f3a7541b17be0d047e8582d2c7adc8d
Steffen Hiller <s.hiller@caprireleases.com>
2009-06-04 13:52:11 -0430

    Matched users controller behaviour to the official php example's controller behaviour.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,85 +1,39 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.xml
+
   def index
     @users = User.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
+    render :json => { :data => @users }
   end
 
-  # GET /users/1
-  # GET /users/1.xml
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.xml
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.xml
   def create
-    @user = User.new(params[:user])
+    @user = User.new(ActiveSupport::JSON.decode(params[:data]))
 
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = 'User was successfully created.'
-        format.html { redirect_to(@user) }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.save
+      render :json => { :success => true, :message => "Created new User #{@user.id}", :data => @user }
+    else
+      render :json => { :message => "Failed to create user"}
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        flash[:notice] = 'User was successfully updated.'
-        format.html { redirect_to(@user) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.update_attributes(ActiveSupport::JSON.decode(params[:data]))
+      render :json => { :success => true, :message => "Updated User #{@user.id}", :data => @user }
+    else
+      render :json => { :message => "Failed to update User"}
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
+    if @user.destroy
+      render :json => { :success => true, :message => "Destroyed User #{@user.id}" }
+    else
+      render :json => { :message => "Failed to destroy User" }
     end
   end
+
 end
